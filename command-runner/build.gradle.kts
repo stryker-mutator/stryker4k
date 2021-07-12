321d6288dc5c9005ee533173655a467f3fa4ab1c
Kees Vaes <vaeskees@gmail.com>
2021-07-12 16:56:16 +0200

    define subproject dependencies

--- command-runner/build.gradle.kts
+++ command-runner/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("io.stryker-mutator:stryker4k-core:0.1.0")
+    implementation(project(":core"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.20")
     testImplementation(kotlin("test-junit"))
     testImplementation("io.mockk:mockk:1.10.6")
